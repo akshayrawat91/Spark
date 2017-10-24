5abb70435ad1c6d916930be20bc577aa48406935
akshay <31359221+akshayrawat91@users.noreply.github.com>
2017-10-24 14:30:37 -0500

    Update mutualFriend.scala

--- mutualFriend.scala
+++ mutualFriend.scala
@@ -1,87 +1,57 @@
-
-package akshay.spark
+//@author akshayrawat91
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
- 
+
 object mutualFriend {
-  
+
   def pairs(line: String) = {
-    val lineSplit = line.split("\t")
+    val lineSplit = line.split("\\W+")
     val user = lineSplit(0)
-    
-    if(lineSplit.length > 1){
-      val friends = lineSplit(1).split(",")
-      val n = friends.length
-      for(i <- 0 to n-1) yield {
-        val pair = if(Integer.parseInt(user) < Integer.parseInt(friends(i))){
-        (user,friends(i))
-        } else {
-          (friends(i),user)
+
+    var friends = ""
+    for( i <- 1 until lineSplit.length) yield {
+      if(i < lineSplit.length -1)
+        friends = friends.concat(lineSplit(i).concat(","))
+      else
+        friends = friends.concat(lineSplit(i))
+    }
+
+    for(i <- 1 until lineSplit.size) yield {
+      val pair = if(Integer.parseInt(user) < Integer.parseInt(lineSplit(i))){
+        (user,lineSplit(i))
+      } else {
+        (lineSplit(i),user)
+      }
+      (pair, friends)
+    }
+
+  }
+
+    def fCount(x: String, y:String) = {
+      val v1 = x.split("\\W+")
+      val v2 = y.split("\\W+")
+      val v1n = v1.length
+      val v2n = v2.length
+      var common = 0
+      for(i <- 0 until v1n; j <- 0 until v2n) yield {
+        if(Integer.parseInt(v1(i)) == Integer.parseInt(v2(j))){
+          common = common +1
         }
-        (pair,lineSplit(1))
-    
       }
-    } 
-    
+      val cm = common.toString()
+      "\t"+cm
+
+    }
+
+  def main(args: Array[String])  {
+    val logFile = "/home/akshay/Documents/bigdata/soc-LiveJournal1Adj.txt" // Should be some file on your system
+    val conf = new SparkConf().setAppName("mutual friend").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile)
+    val mf = logData.flatMap(pairs).reduceByKey(fCount)
+    mf.saveAsTextFile("/home/akshay/Documents/bigdata/assignment2/out1")
+    sc.stop()
   }
-  
-  def mutualCount(x: String, y:String) = {
-    var v1 = x.split("")
-    var v2 = y.split("")
-    if(x.split(",").length > 1){
-      v1 = x.split(",")
-    }
-    if(y.split(",").length > 1){
-      v2 = y.split(",")
-    }
-    val v1n = v1.length
-    val v2n = v2.length
-    var common = 0
-    for(i <- 0 until v1n; j <- 0 until v2n) yield {
-      if(v1(i) == v2(j)){
-        common = common +1        
-      }
-    }
-    val cm = common.toString()
-    "\t"+cm
-  }
-  
-  def main(args: Array[String]) {
-    
-     //Create conf object
-       val conf = new SparkConf()
-     .setAppName("mutualFriend")
-      
-     //create spark context object
-     val sc = new SparkContext(conf)
-     
-    //Check whether sufficient params are supplied
-     if (args.length < 2) {
-     println("Usage: ScalamutualFriend <input> <output>")
-     System.exit(1)
-     }
-     //Read file and create RDD
-     val rawData = sc.textFile(args(0))
-      
-     //convert the lines into words using flatMap operation
-     //val mutualfriend = rawData.map(line => (line.split("\t")(0),line.split("\t")(1).length() ))
-     
-     val mutualfriend = rawData.flatMap(pairs)
-     
-     
-     
-     //val mutualfriend = rawData.map(line => (line.split("\t")(0),line.split("\t")(1)) )
-     
-     
-     
-      
-     //val mutualfriend = words.map(word => (word, 1)).reduceByKey(_ + _)
-      
-     //Save the result
-     mutualfriend.saveAsTextFile(args(1))
-     
-    //stop the spark context
-     sc.stop
- }
+
 }
